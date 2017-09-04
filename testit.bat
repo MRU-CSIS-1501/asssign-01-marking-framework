ee03f80d25e6f742b46e9196d77ff135e0d4278a
Jordan Pratt <jpratt@mtroyal.ca>
2017-09-04 11:03:54 -0600

    Update testit.bat
    Blows away Sonarlint config directory if testing being done for the first time on this project - eliminates some issues with Sonarlint crapping out if old cache files are present. 
    
    Also, speed up Sonarlint a bit by removing the -u flag from the call; by doing so, if this is the first check being run, the server will be contacted and rules will be cached locally, but if it's not the first run, the cached rules will be used.

--- testit.bat
+++ testit.bat
@@ -1,4 +1,10 @@
 @ECHO off
+
+IF EXIST "first-run-done" GOTO process
+rmdir %userprofile%\.sonarlint /s /q
+type nul>first-run-done
+
+:process
 mkdir %userprofile%\.sonarlint\conf
 copy global.json %userprofile%\.sonarlint\conf
 
@@ -11,4 +17,4 @@ javac -d bin -cp "jars/*;bin;." step_definitions/*.java
 
 java -cp "jars/*;bin;" cucumber.api.cli.Main -p html:feature_results --snippets camelcase -g step_definitions features
 
-.\sonarlint\bin\sonarlint -Dsonar.java.source=1.8 -Dsonar.java.libraries="jars/*" --src "**/solution/**" --charset "ISO-8859-1" -u
+.\sonarlint\bin\sonarlint -Dsonar.java.source=1.8 -Dsonar.java.libraries="jars/*" --src "**/solution/**" --charset "ISO-8859-1"  "**/solution/**" --charset "ISO-8859-1" 
