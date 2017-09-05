e1beb979228a7abe8ec7d1e6606a281c16c291d7
Jordan Pratt <jpratt@mtroyal.ca>
2017-09-05 06:36:31 -0600

    Update testit.bat
    Remove stuff left over from copy-paste.

--- testit.bat
+++ testit.bat
@@ -17,4 +17,4 @@ javac -d bin -cp "jars/*;bin;." step_definitions/*.java
 
 java -cp "jars/*;bin;" cucumber.api.cli.Main -p html:feature_results --snippets camelcase -g step_definitions features
 
-.\sonarlint\bin\sonarlint -Dsonar.java.source=1.8 -Dsonar.java.libraries="jars/*" --src "**/solution/**" --charset "ISO-8859-1"  "**/solution/**" --charset "ISO-8859-1" 
+.\sonarlint\bin\sonarlint -Dsonar.java.source=1.8 -Dsonar.java.libraries="jars/*" --src "**/solution/**" --charset "ISO-8859-1"
